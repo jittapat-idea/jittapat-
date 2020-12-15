278b66940ce04e8c8b4d785487950a310a433230
jittapat-idea <76032009+jittapat-idea@users.noreply.github.com>
2020-12-15 14:29:46 +0700

    LED(GET,ON,OFF)

---
+++ esp32_contorl_led_with_MQTT_and_Node-Red/esp32_contorl_led_with_MQTT_and_Node-Red.ino
@@ -0,0 +1,88 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* ssid = "Idea";
+const char* password = "12345678";
+
+// Config MQTT Server
+#define mqtt_server "192.168.43.54"
+#define mqtt_port 1883
+#define mqtt_user "TEST"
+#define mqtt_password "12345"
+
+#define LED_PIN 2
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe("/ESP/LED");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+      return;
+    }
+  }
+  client.loop();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String msg = "";
+  int i=0;
+  while (i<length) msg += (char)payload[i++];
+  if (msg == "GET") {
+    client.publish("/ESP/LED", (digitalRead(LED_PIN) ? "LEDON" : "LEDOFF"));
+    Serial.println("Send !");
+    return;
+  }
+  if (msg == "on") {
+    client.publish("/ESP/LED", "state-on");
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Send on !");
+    return;
+  }
+  if (msg == "off") {
+    client.publish("/ESP/LED", "state-off");
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("Send off !");
+    return;
+  }
+  //digitalWrite(LED_PIN, (msg == "LEDON" ? HIGH : LOW));
+ // Serial.println(msg);
+}
